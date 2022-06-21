d9093299687f370d06d5759e64b8c9e585f24834
Dominika <dominika.d.gacek@gmail.com>
2022-06-21 20:35:42 +0200

    Nopwy commit

---
+++ src/components/NavigationHeader.js
@@ -0,0 +1,27 @@
+import classes from "./NavigationHeader.module.css";
+import {Link} from "react-router-dom";
+
+const NavigationHeader = () =>{
+    return(<header className={classes.AppHeader}>
+        <div className={classes.PasekNawigacyjny}>
+            <div className={classes.Lewa}>
+                <div className={classes.TextLewa}>SDA</div>
+            </div>
+            <div className={classes.Prawa}>
+                <div className={classes.TextPrawa}>
+                    <p>ARPPL4</p>
+                </div>
+                <p>
+                    <Link to={"/"}>Home</Link>
+                </p>
+                <p>
+                    <Link to={"/list"}>List</Link>
+                </p>
+            </div>
+
+        </div>
+    </header>
+    );
+
+}
+export default NavigationHeader;
